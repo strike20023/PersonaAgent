b511e38cf518007b4baf823e4423f12ba6532495
Herman Huang <35003938+strike20023@users.noreply.github.com>
2025-11-03 11:46:43 +0000

    添加准备benchmark数据集的脚本

---
+++ prepare_bench.sh
@@ -0,0 +1,5 @@
+##准备benchmark数据集
+git lfs install
+git clone https://huggingface.co/datasets/bowen-upenn/ImplicitPersona ./data/ImplicitPersona
+
+git clone https://huggingface.co/datasets/bowen-upenn/PersonaMem ./data/PersonaMem
